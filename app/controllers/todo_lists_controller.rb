76767edd6c6bfcb9f74234bb04886dbf69ab65ca
Vinuta M Hiremath <vinuta123.hiremath@gmail.com>
2026-05-26 12:09:19 +0530

    Added show to view details (#6)
    * Added show to view details
    
    * added extra line

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,5 +1,14 @@
 class TodoListsController < ApplicationController
+  before_action :set_todo_list, only: [ :show ]
+
   def index
     @todo_lists = TodoList.all
+  end
+
+  def show
+  end
+
+  def set_todo_list
+    @todo_list = TodoList.find(params[:id])
   end
 end
