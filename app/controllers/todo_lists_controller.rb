e0db0565049c43a5fdcf3e56013591e3355794c5
Vinuta M Hiremath <vinuta123.hiremath@gmail.com>
2026-05-26 13:38:04 +0530

    Todolist new and create (#7)
    * Added show to view details
    
    * added extra line
    
    * Added create
    
    * Added validation for the name

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -8,7 +8,25 @@ class TodoListsController < ApplicationController
   def show
   end
 
+  def new
+    @todo_list = TodoList.new
+  end
+
+  def create
+    @todo_list = TodoList.new(set_todo_params)
+    if @todo_list.save
+      redirect_to todo_lists_path, notice: "Todo created successfully"
+    else
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  private
   def set_todo_list
     @todo_list = TodoList.find(params[:id])
+  end
+
+  def set_todo_params
+    params.require(:todo_list).permit(:name, :description)
   end
 end
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,2 +1,3 @@
 class TodoList < ApplicationRecord
+  validates :name, presence: true
 end
